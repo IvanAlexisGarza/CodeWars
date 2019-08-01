fca5035973b340a616ef3f5124f59b5862de4064
Garza, Alexis <alexis.garza@thermofisher.com>
2019-08-01 11:50:55 -0700

    Finishing insertionSort

---
+++ Algorithms and data structures/exersices/insertionSort.js
@@ -0,0 +1,18 @@
+function insertionSort(arr){
+	var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+}
+
+insertionSort([2,1,9,76,4])
+
+let array = [1, 5, 8, 4, 7, 9, 3]; 
+// console.log(array);
+
+console.log(insertionSort(array));
--- Algorithms and data structures/exersices/selectionSort.js
+++ Algorithms and data structures/exersices/selectionSort.js
@@ -1,3 +1,27 @@
 function selectionSort(arr) {
-    
+    let smallest = arr[0];
+    let index;
+
+    for (let i = 0; i < arr.length-1; i++) {
+        let smallest = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[smallest]) {
+                smallest = j;
+            }        
+        }
+        if(i !== smallest) {
+            [arr[i] , arr[smallest]] = [arr[smallest], arr[i]];
+        }
+    }
+
+    return arr;
 }
+
+// function swap(a,b) {
+//     b = (a += b -= a) -b;
+// }
+
+// var array = [1,3,2,7,4,4,8];
+var array = [999991,23,2568,5477,46,544,83];
+
+console.log(selectionSort(array))
